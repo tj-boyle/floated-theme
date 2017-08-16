fec6e1dc91703603d1e80d5bcd9ebe0ee5ed666a
TJ Boyle <thomas.john.boyle@gmail.com>
2017-08-15 23:07:51 -0400

    Fullscreen slider, category pages, font changes

--- category.php
+++ category.php
@@ -1,13 +1,27 @@
 <?php get_header(); ?>
 <section id="content" role="main">
-<header class="header">
-<h1 class="entry-title"><?php _e( 'Category Archives: ', 'blankslate' ); ?><?php single_cat_title(); ?></h1>
-<?php if ( '' != category_description() ) echo apply_filters( 'archive_meta', '<div class="archive-meta">' . category_description() . '</div>' ); ?>
-</header>
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<?php get_template_part( 'entry' ); ?>
-<?php endwhile; endif; ?>
-<?php get_template_part( 'nav', 'below' ); ?>
+	<header class="header">
+		<h1 class="title"><?php _e( 'Category Archives: ', 'blankslate' ); ?><?php single_cat_title(); ?></h1>
+		<?php if ( '' != category_description() ) echo apply_filters( 'archive_meta', '<div class="archive-meta">' . category_description() . '</div>' ); ?>
+	</header>
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+		<div class="entry">
+			<?php if (has_post_thumbnail()):?>
+				<div class="entry-thumbnail">
+					<?php the_post_thumbnail(); ?>
+				</div>
+			<?php endif;?>
+			<div class="entry-words">
+				<div class="cat-links"><?php the_category( ', ' ); ?></div>
+				<h1 class="entry-title">
+					<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark">
+						<?php the_title(); ?>
+					</a>
+				</h1>
+				<?php the_excerpt();?>
+			</div>
+		</div>
+	<?php endwhile; endif; ?>
 </section>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- entry-content.php
+++ entry-content.php
@@ -1,19 +1,42 @@
 <section class="entry-content">
 	<?php if ( is_singular() ) : ?>
-		<?php if (has_post_thumbnail()):?>
+		<?php 
+			function debug($msg) {
+				$msg = str_replace('"', '\\"', $msg); // Escaping double quotes 
+				echo "<script>console.log(\"$msg\")</script>";
+			}
+
+			$tags = get_the_tags();
+			$sliderNum = -1;
+			if($tags){
+				foreach($tags as $tag){
+					$pos = strpos($tag->name, '[masterslider id="');
+					if($pos === 0){
+						$sliderNum = explode('"', $tag->name)[1];
+					}
+				}
+			}
+
+			if($sliderNum > -1)	{
+				masterslider($sliderNum);
+			}
+		?>
+		<?php if (has_post_thumbnail() && $sliderNum === -1):?>
 			<div class="entry-thumbnail">
 				<div class="thumbnail-div" style="background-image: url('<?php echo(get_the_post_thumbnail_url(null, [768,282])); ?>') "></div>
 			</div>
 		<?php endif;?>
 		<div class="entry-container">
 			<div class="entry-words">
-				<h1 class="entry-title">
-					<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark">
-						<?php the_title(); ?>
-					</a>
+				<div class="entry-header">
+						<h1 class="entry-title">
+						<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark">
+							<?php the_title(); ?>
+						</a>
+					</h1>
 					<?php edit_post_link(); ?>
 					<?php if ( !is_search() ) get_template_part( 'entry', 'meta' ); ?>
-				</h1>
+				</div>
 				<?php the_content();?>
 			</div>
 			<div class="entry-links">
